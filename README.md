b966e9236b34da10ca32027ed2c9c0c61e973f62
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-08-05 14:43:40 +0600

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# hidden-object-server
---
+++ index.js
@@ -0,0 +1,47 @@
+const { Server } = require("socket.io");
+require('dotenv').config()
+const { instrument } = require("@socket.io/admin-ui");
+const io = new Server(process.env.PORT, {
+  cors: {
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL2, "https://admin.socket.io"],
+    credentials: true
+  }
+});
+
+io.on('connect', (socket) => {
+  // console.log('A user connected: ', socket.id);
+
+  socket.on("host-game", (gameCode, username, hostCallBack) => {
+    socket.nickname = username
+    socket.join(gameCode)
+    hostCallBack("Waiting for others to join...")
+  })
+
+  socket.on("join-game", async (gameCode, username, questionSet, joinCallBack) => {
+    const sockets = await io.in(gameCode).fetchSockets();
+
+    if (sockets.length == 1) {
+      socket.nickname = username
+      socket.join(gameCode)
+      socket.to(gameCode).emit("server-question", questionSet)
+      io.in(gameCode).emit("other-joined", "Game Start")
+    } else {
+      joinCallBack("The Game is not hosted yet or already it's started.")
+    }
+  })
+
+  socket.on("add-score", (gameCode, score, correctCount, opponentHighScore) => {
+    // console.log("Add Score", socket.nickname, score, correctCount, opponentHighScore)
+    socket.to(gameCode).emit("show-score", score, correctCount, opponentHighScore)
+  })
+
+  socket.on("game-over", (gameCode) => {
+    // console.log("Game Over")
+    socket.to(gameCode).emit("over-show")
+  })
+
+});
+
+instrument(io, {
+  auth: false
+});
